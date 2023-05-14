d172ab8892860d4f4be035f1f9a05f70d37cc83a
brandoxcode <brandon.a2001@outlook.es>
2023-05-13 21:12:01 -0400

    Fixed API again

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -1,6 +1,6 @@
 import { Table } from "sst/node/table";
-import handler from "@notes/core/handler";
-import dynamoDb from "@notes/core/dynamodb";
+import handler from "@notes/core/src/handler";
+import dynamoDb from "./dynamodb";
 
 export const main = handler(async (event) => {
     const params = {
