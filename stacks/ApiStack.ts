ad2c754fe01934b94b90345eb2f6a0fbdd72fd5b
brandoxcode <brandon.a2001@outlook.es>
2023-05-13 21:09:46 -0400

    Fixed API

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,42 @@
+import { Api, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack(input: ApiStackInp): ApiStackRes {
+    const { stack, app } = input
+    const { table } = use(StorageStack);
+
+    // Create the API
+    const api = new Api(stack, "Api", {
+        defaults: {
+            function: {
+                bind: [table],
+            },
+        },
+        routes: {
+            "GET /notes": "packages/functions/src/list.main",
+            "POST /notes": "packages/functions/src/create.main",
+            "GET /notes/{id}": "packages/functions/src/get.main",
+            "PUT /notes/{id}": "packages/functions/src/update.main",
+            "DELETE /notes/{id}": "packages/functions/src/delete.main",
+        },
+    });
+
+    // Show the API endpoint in the output
+    stack.addOutputs({
+        ApiEndpoint: api.url,
+    });
+
+    // Return the API resource
+    return {
+        api,
+    };
+}
+
+export type ApiStackInp = {
+    stack: any;
+    app: any;
+}
+
+export type ApiStackRes = {
+    api: Api;
+}
